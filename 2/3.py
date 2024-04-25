a898c280e9ce44bca677da61ee69320a2eaac8e1
alexknyaz123 <168131017+alexknyaz123@users.noreply.github.com>
2024-04-25 22:49:20 +0300

    Create 3.py

---
+++ 2/3.py
@@ -0,0 +1,12 @@
+x = 7
+y = 6 
+z = - 18 
+positive_numbers = []
+if x > 0:
+    positive_numbers.append(x)
+if y > 0:
+    positive_numbers.append(y)
+if z > 0:
+    positive_numbers.append(z)
+print("Положительные числа:", positive_numbers)
+print("Количество положительных чисел:", len(positive_numbers))
